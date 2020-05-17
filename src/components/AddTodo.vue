64bd0a935e4914d47531c4b0b8baa9ea73a2aa43
Paul D. Mendes <pauldmendes5@gmail.com>
2020-05-17 21:17:49 +0400

    Initial Commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+    <input type="submit" value="Submit" class="btn" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: Date.now(),
+        title: this.title,
+        completed: false
+      };
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    }
+  },
+
+  data() {
+    return {
+      title: ""
+    };
+  }
+};
+</script>
+
+<style>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h1>Todo List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: darkblue;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
